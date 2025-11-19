b427dc9a1c20fef0867ec54356c23f9a7b94d95b
Prokhor Ozornin <prokhor.ozornin@yandex.ru>
2025-11-19 19:24:12 +0500

    Extensions blocks

--- src/W3CValidator/Css/ICssRequestExecutorExtensions.cs
+++ src/W3CValidator/Css/ICssRequestExecutorExtensions.cs
@@ -6,33 +6,35 @@
 /// <seealso cref="ICssRequestExecutor"/>
 public static class ICssRequestExecutorExtensions
 {
-  /// <summary>
-  ///   <para></para>
-  /// </summary>
   /// <param name="executor"></param>
-  /// <param name="document"></param>
-  /// <returns></returns>
-  /// <exception cref="ArgumentNullException">If either <paramref name="executor"/> or <paramref name="document"/> is <see langword="null"/>.</exception>
-  public static ICssValidationResult Document(this ICssRequestExecutor executor, string document)
+  extension(ICssRequestExecutor executor)
   {
-    if (executor is null) throw new ArgumentNullException(nameof(executor));
-    if (document is null) throw new ArgumentNullException(nameof(document));
+    /// <summary>
+    ///   <para></para>
+    /// </summary>
+    /// <param name="document"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException">If either <paramref name="executor"/> or <paramref name="document"/> is <see langword="null"/>.</exception>
+    public ICssValidationResult Document(string document)
+    {
+      if (executor is null) throw new ArgumentNullException(nameof(executor));
+      if (document is null) throw new ArgumentNullException(nameof(document));
 
-    return executor.DocumentAsync(document).Result;
-  }
+      return executor.DocumentAsync(document).Result;
+    }
 
-  /// <summary>
-  ///   <para></para>
-  /// </summary>
-  /// <param name="executor"></param>
-  /// <param name="url"></param>
-  /// <returns></returns>
-  /// <exception cref="ArgumentNullException">If either <paramref name="executor"/> or <paramref name="url"/> is <see langword="null"/>.</exception>
-  public static ICssValidationResult Url(this ICssRequestExecutor executor, Uri url)
-  {
-    if (executor is null) throw new ArgumentNullException(nameof(executor));
-    if (url is null) throw new ArgumentNullException(nameof(url));
+    /// <summary>
+    ///   <para></para>
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException">If either <paramref name="executor"/> or <paramref name="url"/> is <see langword="null"/>.</exception>
+    public ICssValidationResult Url(Uri url)
+    {
+      if (executor is null) throw new ArgumentNullException(nameof(executor));
+      if (url is null) throw new ArgumentNullException(nameof(url));
 
-    return executor.UrlAsync(url).Result;
+      return executor.UrlAsync(url).Result;
+    }
   }
 }
--- src/W3CValidator/Css/ICssValidationRequestExtensions.cs
+++ src/W3CValidator/Css/ICssValidationRequestExtensions.cs
@@ -9,39 +9,39 @@ namespace W3CValidator.Css;
 /// <seealso cref="ICssValidationRequest"/>
 public static class ICssValidationRequestExtensions
 {
-  /// <summary>
-  ///   <para>Specifies language locale/culture to be used for description of validation issues.</para>
-  /// </summary>
   /// <param name="request">Validation request instance.</param>
-  /// <param name="culture">Text culture.</param>
-  /// <returns>Back reference to the provided validation <paramref name="request"/>.</returns>
-  /// <exception cref="ArgumentNullException">If <paramref name="request"/> is <see langword="null"/>.</exception>
-  public static ICssValidationRequest Language(this ICssValidationRequest request, CultureInfo culture) => request is not null ? request.Language(culture?.TwoLetterISOLanguageName) : throw new ArgumentNullException(nameof(request));
+  extension(ICssValidationRequest request)
+  {
+    /// <summary>
+    ///   <para>Specifies language locale/culture to be used for description of validation issues.</para>
+    /// </summary>
+    /// <param name="culture">Text culture.</param>
+    /// <returns>Back reference to the provided validation <paramref name="request"/>.</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="request"/> is <see langword="null"/>.</exception>
+    public ICssValidationRequest Language(CultureInfo culture) => request is not null ? request.Language(culture?.TwoLetterISOLanguageName) : throw new ArgumentNullException(nameof(request));
 
-  /// <summary>
-  ///   <para>Specifies CSS medium to use in validation process.</para>
-  /// </summary>
-  /// <param name="request">Validation request instance.</param>
-  /// <param name="medium">CSS medium.</param>
-  /// <returns>Back reference to the provided validation <paramref name="request"/>.</returns>
-  /// <exception cref="ArgumentNullException">If <paramref name="request"/> is <see langword="null"/>.</exception>
-  public static ICssValidationRequest Medium(this ICssValidationRequest request, CssMedium? medium) => request is not null ? request.Medium(medium?.ToInvariantString()) : throw new ArgumentNullException(nameof(request));
+    /// <summary>
+    ///   <para>Specifies CSS medium to use in validation process.</para>
+    /// </summary>
+    /// <param name="medium">CSS medium.</param>
+    /// <returns>Back reference to the provided validation <paramref name="request"/>.</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="request"/> is <see langword="null"/>.</exception>
+    public ICssValidationRequest Medium(CssMedium? medium) => request is not null ? request.Medium(medium?.ToInvariantString()) : throw new ArgumentNullException(nameof(request));
 
-  /// <summary>
-  ///   <para>Specifies CSS standard version (profile) to be used in validation process.</para>
-  /// </summary>
-  /// <param name="request">Validation request instance.</param>
-  /// <param name="profile">CSS profile.</param>
-  /// <returns>Back reference to the provided validation <paramref name="request"/>.</returns>
-  /// <exception cref="ArgumentNullException">If <paramref name="request"/> is <see langword="null"/>.</exception>
-  public static ICssValidationRequest Profile(this ICssValidationRequest request, CssProfile? profile) => request is not null ? request.Profile(profile?.ToString().ToLowerInvariant()) : throw new ArgumentNullException(nameof(request));
+    /// <summary>
+    ///   <para>Specifies CSS standard version (profile) to be used in validation process.</para>
+    /// </summary>
+    /// <param name="profile">CSS profile.</param>
+    /// <returns>Back reference to the provided validation <paramref name="request"/>.</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="request"/> is <see langword="null"/>.</exception>
+    public ICssValidationRequest Profile(CssProfile? profile) => request is not null ? request.Profile(profile?.ToString().ToLowerInvariant()) : throw new ArgumentNullException(nameof(request));
 
-  /// <summary>
-  ///   <para>Specifies level of warnings to be included. Only the ones whose level is under or equal to the value specified in the request will be displayed.</para>
-  /// </summary>
-  /// <param name="request">Validation request instance.</param>
-  /// <param name="level">Level of warnings severity.</param>
-  /// <returns>Back reference to the provided validation <paramref name="request"/>.</returns>
-  /// <exception cref="ArgumentNullException">If <paramref name="request"/> is <see langword="null"/>.</exception>
-  public static ICssValidationRequest Warnings(this ICssValidationRequest request, WarningsLevel? level) => request is not null ? request.Warnings((int?) level) : throw new ArgumentNullException(nameof(request));
+    /// <summary>
+    ///   <para>Specifies level of warnings to be included. Only the ones whose level is under or equal to the value specified in the request will be displayed.</para>
+    /// </summary>
+    /// <param name="level">Level of warnings severity.</param>
+    /// <returns>Back reference to the provided validation <paramref name="request"/>.</returns>
+    /// <exception cref="ArgumentNullException">If <paramref name="request"/> is <see langword="null"/>.</exception>
+    public ICssValidationRequest Warnings(WarningsLevel? level) => request is not null ? request.Warnings((int?) level) : throw new ArgumentNullException(nameof(request));
+  }
 }
